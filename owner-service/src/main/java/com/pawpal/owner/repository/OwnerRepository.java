1390c2e175002f9d254c44b6a49da60e3e711745
Aalyahh <g00097023@aus.edu>
2026-04-12 15:39:25 +0400

    Delete owner-service directory

--- owner-service/src/main/java/com/pawpal/owner/repository/OwnerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pawpal.owner.repository;
-
-import com.pawpal.owner.model.Owner;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OwnerRepository extends JpaRepository<Owner, Integer> {
-}
